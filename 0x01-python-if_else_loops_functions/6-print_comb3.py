6988ede17bf92fcf82dfb4e037c79a8960969b35
Apiyo120 <apiyodianah@gmail.com>
2023-06-06 18:53:04 -0700

    Loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,7 +4,7 @@
 The two digits should be different and are considered identical."""
 
 for num1 in range(0, 10):
-    for num2 in range(num1 +1, 10):
+    for num2 in range(num1 + 1, 10):
         if num1 == 8 and num2 == 9:
             print("{}{}" .format(num1, num2))
         else:
