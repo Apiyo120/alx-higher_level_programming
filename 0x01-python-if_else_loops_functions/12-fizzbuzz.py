063bab2fb97ad550deb6b64de216a01ae054e56a
Apiyo120 <apiyodianah@gmail.com>
2023-06-07 05:44:56 -0700

    Loops

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+
+    """Print a functio that print number from 1 - 100."""
+
+    for number in range(1, 100):
+        if number % 3 == 0:
+            print("Fizz", end="")
+
+        elif number % 5 == 0:
+            print("Buzz", end="")
+
+        elif number % 3 and number % 5 == 0:
+            print("FizzBuzz", end="")
+
+        else:
+            print("{}" .format(number), end="")
+
