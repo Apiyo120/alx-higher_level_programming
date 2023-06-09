19b2e3d3f2e1a34940e794faaf60d3b6999b5d8e
Apiyo120 <apiyodianah@gmail.com>
2023-06-08 17:56:22 -0700

    Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
 
     """To print the number of list arguments."""
     
-    iport sys
+    import sys
 
     count = len(sys.argv) - 1
     if count == 0:
@@ -16,5 +16,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:" .format(count))
 
-    for i in range(count)
-    print("{}: {}" .format(i + 1, sys.argv[i +1]))
+    for i in range(count):
+        print("{} : {}" .format(i + 1, sys.argv[i +1]))
