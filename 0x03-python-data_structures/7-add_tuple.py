01253166d1a6cc791976e501e8e06c1882f0320b
Apiyo120 <apiyodianah@gmail.com>
2023-06-12 18:13:53 -0700

    Data_structure

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,7 +2,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
 
-    if len(tuple_a) < 2:
+    if len(tple_a) < 2:
         if len(tuple_a) == 0:
             tuple_a = 0, 0
 
@@ -15,4 +15,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
         else:
             tuple_b = tuple_b[0], 0
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
+
+    return (tupple_a[0] + tule_b[0], tuple_a[1] + tuple_b[1]
