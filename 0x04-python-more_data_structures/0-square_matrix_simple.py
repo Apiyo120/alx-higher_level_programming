67740056a86a5d9a6cd3d6e634c34dd6385a1650
Apiyo120 <apiyodianah@gmail.com>
2023-06-15 04:23:50 -0700

    Data_structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-0-square_matrix_simple.py
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
 
-new_matrix = matrix.copy()
+    for i in range(len(matrix)):
+        new_matrix[i = list(map(lambda x: x ** 2, matrix[i]))
 
-for i in range(len(matrix):
-        new_matrix[i]=list(lambda x: X**2, matrix[i]))
-
-return(new_matrix)
+    return (new_matrix)
