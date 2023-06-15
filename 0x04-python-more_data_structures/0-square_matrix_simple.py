c80625b696b9935928741531987c746dd874cf67
Apiyo120 <apiyodianah@gmail.com>
2023-06-14 18:21:16 -0700

    Data_structures

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+0-square_matrix_simple.py
+
+new_matrix = matrix.copy()
+
+for i in range(len(matrix):
+        new_matrix[i]=list(lambda x: X**2, matrix[i]))
+
+return(new_matrix)
---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/bash/python3
+
+def update_dictionary(a_dictionary, key, value):
+    # replace or add key/value pairs in a disctionary.
+    a_dictionary[key] = value
+
+    return (a_dictionary)
