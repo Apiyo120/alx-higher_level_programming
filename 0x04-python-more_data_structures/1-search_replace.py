de64b7dbc02c0229576b472b8ba6d7e6fff1ca3b
Apiyo120 <apiyodianah@gmail.com>
2023-06-14 19:05:05 -0700

    Data_structures

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/pyton3
 
 def search_replace(my_list, search, replace):
-    new_kist = list(map(lambda x: replace if x == search else x, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return(new_list)
