4dc029530cfa7d90e1be6c8c75075b2929c0cb51
Apiyo120 <apiyodianah@gmail.com>
2023-06-26 01:54:08 -0700

    Modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,10 @@
 if __name__ == "__main__":
-   
     """To print all names defined by hidden_4 module."""
     import hidden_4
 
     names = dir(hidden_4)
 
     for name in names:
-        if name[:2] != "__":
+        if not name.startswith("__"):
 
             print(name)
