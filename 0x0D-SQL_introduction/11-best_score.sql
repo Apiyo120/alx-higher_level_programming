2f4e111aebacf832c4c66e43a965316bc549fc6a
Apiyo120 <apiyodianah@gmail.com>
2023-08-19 01:00:32 -0700

    SQL introductions

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- Lists all records in the 'second_table' with a score >= 10 in MySQL.
+-- The records are ordered in a descending order.
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
