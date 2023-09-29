6951c67252a5edb3dec0ef2fe6986f919c948836
Apiyo120 <apiyodianah@gmail.com>
2023-09-29 12:11:17 -0700

    Networkk

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Posts an email"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    print("{}".format(r.text))
